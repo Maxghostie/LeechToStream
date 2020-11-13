f3b4a9b2bd28cf866ea0af420db078c691d6015e
Maxghostie <70423687+Maxghostie@users.noreply.github.com>
2020-11-13 12:26:39 +0530

    Update sample_config.py

--- tobrot/sample_config.py
+++ tobrot/sample_config.py
@@ -2,14 +2,14 @@ import os
 
 class Config(object):
     # get a token from @BotFather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1492824147:AAHJjugUae2eXQLIZuq4N9hVfkkw6qSs6jY")
     # The Telegram API things
-    APP_ID = int(os.environ.get("APP_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
-    OWNER_ID = int(os.environ.get("OWNER_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", 948659))
+    API_HASH = os.environ.get("fbc5894ad4154499f90bba0421b43359")
+    OWNER_ID = int(os.environ.get("OWNER_ID", 1123693059))
     # Get these values from my.telegram.org
     # to store the channel ID who are authorized to use the bot
-    AUTH_CHANNEL = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "").split())
+    AUTH_CHANNEL = set(int(x) for x in os.environ.get("AUTH_CHANNEL", "-1001335498211").split())
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     # Telegram maximum file upload size
@@ -34,7 +34,7 @@ class Config(object):
     UN_FINISHED_PROGRESS_STR = os.environ.get("UN_FINISHED_PROGRESS_STR", "░")
     # add offensive API
     TG_OFFENSIVE_API = os.environ.get("TG_OFFENSIVE_API", None)
-    CUSTOM_FILE_NAME = os.environ.get("CUSTOM_FILE_NAME", "")
+    CUSTOM_FILE_NAME = os.environ.get("CUSTOM_FILE_NAME", "[WizTX]")
     LEECH_COMMAND = os.environ.get("LEECH_COMMAND", "leech")
     YTDL_COMMAND = os.environ.get("YTDL_COMMAND", "ytdl")
     PYTDL_COMMAND = os.environ.get("PYTDL_COMMAND", "pytdl")
